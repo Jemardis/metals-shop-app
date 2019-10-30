6cc1f599f6d518ab91d4ec4157faffcef05e32f2
Jim Mardis <james.mardis@boone.kyschools.us>
2019-10-29 21:29:40 -0400

    modified Header.js and .css

--- src/Header.js
+++ src/Header.js
@@ -9,22 +9,44 @@ class Header extends Component {
     this.state = {};
   }
 
+  handleClick(event) {
+    let buttonText = event.target.innerHTML;
+    //buttonText === "Log in"
+
+    if (buttonText === "Log in") {
+      alert("You've clicked the log in button");
+    } else {
+      // buttonText === "View Shopping Cart"
+      // we've clicked the shopping cart button
+      // have alert with different message
+      alert("You've clicked the shopping cart button");
+    }
+  }
+
   render() {
     return (
       <div className="header-container">
         <img src={metalslogo} alt="metals-logo" />
-          <div className="header-description">Metals Trading</div>
-          <button className="header-buttons">Log in</button>
-          <div className="shopping-cart-container">
-            <img
-              className="shopping-card-logo"
-              src={shoppingcart}
-              alt="shopping-cart-logo"
-            />
-            <button className="header-buttons shopping-cart-button">
-              View Shopping Cart
-            </button>
-          </div>
+        <div className="header-description">Metals Trading</div>
+        <button
+          className="header-buttons log-in-button"
+          onClick={this.handleClick}
+        >
+          Log in
+        </button>
+        <div className="shopping-cart-container">
+          <img
+            className="shopping-card-logo"
+            src={shoppingcart}
+            alt="shopping-cart-logo"
+          />
+          <button
+            className="header-buttons shopping-cart-button"
+            onClick={this.handleClick}
+          >
+            View Shopping Cart
+          </button>
+        </div>
       </div>
     );
   }
